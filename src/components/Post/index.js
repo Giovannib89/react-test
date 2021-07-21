3332eca134078274633ec9b192ef24d0ecf250e2
Giovannib89 <brugnoni.giovanni89@gmail.com>
2021-07-21 20:23:30 +0200

    gestion du clique sur un Post

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import DOMPurify from 'dompurify';
+// import de Link dans react-router-dom
+import { Link } from 'react-router-dom';
 
 import './styles.scss';
 
-const Post = ({ category, excerpt, title }) => {
+// ajout de la props slug au composant post
+const Post = ({
+  category,
+  excerpt,
+  title,
+  slug,
+}) => {
   const createMarkup = (text) => {
     const cleanText = DOMPurify.sanitize(text, { ALLOWED_TAGS: ['em', 'strong'] });
 
     return { __html: cleanText };
   };
 
+  //! Test= appuis sur un article pour étre redirigé vers une page 404
+  // link va permetre de créer une nouvelle route 
+  // ainsi que de transformé les article en lien clicable 
+  // pour afficher par la suite le composant single
   return (
-    <article className="post">
-      <h2 className="post-title">{title}</h2>
-      <div className="post-category">{category}</div>
-      <p
-        className="post-excerpt"
-        dangerouslySetInnerHTML={createMarkup(excerpt)}
-      />
-    </article>
+    <Link to={`/post/${slug}`} className="post">
+      <article>
+        <h2 className="post-title">{title}</h2>
+        <div className="post-category">{category}</div>
+        <p
+          className="post-excerpt"
+          dangerouslySetInnerHTML={createMarkup(excerpt)}
+        />
+      </article>
+    </Link>
   );
 };
 
@@ -27,6 +41,8 @@ Post.propTypes = {
   category: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   excerpt: PropTypes.string.isRequired,
+  // ajout de slug dans les prop types en string
+  slug: PropTypes.string.isRequired,
 };
 
 export default Post;
