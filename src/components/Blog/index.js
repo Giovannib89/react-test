3fffa7b2dfb0e573bb70336f618bb2e2624e15a6
Giovannib89 <brugnoni.giovanni89@gmail.com>
2021-07-21 18:36:20 +0200

    mise en place du faux chargement des posts

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -5,7 +5,6 @@ import Header from 'src/components/Header';
 import Posts from 'src/components/Posts';
 import Footer from 'src/components/Footer';
 import NotFound from 'src/components/NotFound';
-// import du composant Spinner
 import Spinner from 'src/components/Spinner';
 
 import categoriesData from 'src/data/categories';
@@ -15,22 +14,39 @@ import './styles.scss';
 const Blog = () => {
   const [zenMode, setZenMode] = useState(false);
 
-  // on prépare une nouvelle dans le state pour gérer l'état de loading
-  //! change true en false pour voir le résultat
-  // ou dans le navigateur dans components au niveau de Blog décoche le hooks sur true
-  const [loading, setLoading] = useState(true);
+  // on passe manuellement loading a false
+  const [loading, setLoading] = useState(false);
+
+  // on stocke désormais les données des posts dans le state
+  const [posts, setPosts] = useState([]);
 
   const toggleZenMode = () => {
     setZenMode(!zenMode);
   };
 
-  const getPostsByCategory = (category, posts) => {
+  // on change posts en postList
+  const getPostsByCategory = (category, postsList) => {
     if (category === 'Accueil') {
-      return posts;
+      return postsList;
     }
-
-    const filteredPosts = posts.filter((post) => post.category === category);
+    // ici aussi
+    const filteredPosts = postsList.filter((post) => post.category === category);
     return filteredPosts;
+  };
+
+  // création d'une fonction qui va permettre par la suite d'aller chercher
+  // les données d'une API
+  const loadData = () => {
+    // on met le spinner en route
+    setLoading(true);
+
+    // on simule le chargement
+    setTimeout(() => {
+      // on arrête le spinner
+      setLoading(false);
+      // on vient remplir le state avec les données en local
+      setPosts(postsData);
+    }, 2000);
   };
 
   return (
@@ -40,12 +56,11 @@ const Blog = () => {
         onClickZenButton={toggleZenMode}
         isZen={zenMode}
       />
-      {/* ajout d'un bouton qui permet d'envoyer les donnés */}
-      <button type="button" onClick={() => console.log('je veux charger les données')}>Load Data</button>
-      {/* quand loading est true ça lance le spinner */}
+      {/* au click on lance la fonction loaddata qui va lancer le spinner 
+      pendant 3 seconde et ensuite afficher les datas */}
+      <button type="button" onClick={loadData}>Load Data</button>
       {loading && <Spinner />}
 
-      {/* lorsque loading passe a false affichage des article */}
       {!loading && (
       <Switch>
         {categoriesData.map(({ route, label }) => (
@@ -54,7 +69,8 @@ const Blog = () => {
             exact
             path={route}
           >
-            <Posts posts={getPostsByCategory(label, postsData)} isZen={zenMode} />
+            {/* on retire postData et on place le state posts  */}
+            <Posts posts={getPostsByCategory(label, posts)} isZen={zenMode} />
           </Route>
         ))}
         <Route path="/angular">Ne s'affiche pas</Route>
