35c7f73e48dd731ba7c1b02ee41187c59510398d
Giovannib89 <brugnoni.giovanni89@gmail.com>
2021-07-21 18:13:25 +0200

    ajout de la 404 avec Switch

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,10 +1,12 @@
-// == Import
 import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
+// ajout de l'import Switch dans react-router-dom
+import { Route, Switch } from 'react-router-dom';
 
 import Header from 'src/components/Header';
 import Posts from 'src/components/Posts';
 import Footer from 'src/components/Footer';
+// importation du composant NotFound
+import NotFound from 'src/components/NotFound';
 
 import categoriesData from 'src/data/categories';
 import postsData from 'src/data/posts';
@@ -17,11 +19,8 @@ const Blog = () => {
     setZenMode(!zenMode);
   };
 
-  // on ajoute la props posts qui va nous servir a afficher tout les article pour l'Accueil
   const getPostsByCategory = (category, posts) => {
-    // on utilise un if qui dit si categori et egal a Accueil
     if (category === 'Accueil') {
-      // alors nous envoyon tte les datas
       return posts;
     }
 
@@ -36,17 +35,33 @@ const Blog = () => {
         onClickZenButton={toggleZenMode}
         isZen={zenMode}
       />
+      {/*
+        Switch permet d'afficher le 1e composant Route
+        dont le path correspondra avec le pathname de l'url
+       */}
+      <Switch>
+        {/*
+          ici on génère un composant Route par catégorie
+          pour chacun de ses composants, on va mettre en enfant
+          un composant "Posts" avec en props "posts" une liste
+          filtrée de postsData. Filtrée en fonction de category.label
+        */}
+        {categoriesData.map(({ route, label }) => (
+          <Route
+            key={label}
+            exact
+            path={route}
+          >
+            <Posts posts={getPostsByCategory(label, postsData)} isZen={zenMode} />
+          </Route>
+        ))}
+        {/* ce composant Route ne s'affichera jamais, il y en a un avant lui */}
+        <Route path="/angular">Ne s'affiche pas</Route>
 
-      {/* Ajout d'une key pour chaque route on utilise ici le label et on destructure catégori */}
-      {categoriesData.map(({ route, label }) => (
-        <Route
-          key={label}
-          exact
-          path={route}
-        >
-          <Posts posts={getPostsByCategory(label, postsData)} isZen={zenMode} />
-        </Route>
-      ))}
+        {/* si on veut mettre en place une 404, on ajoute
+        une composant Route sans props "path" à la fin d'un Switch */}
+        <Route><NotFound /></Route>
+      </Switch>
       <Footer />
     </div>
   );
