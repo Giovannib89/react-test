4387e6f41f4269526a5e8390763eb03b22ec1e97
Giovannib89 <brugnoni.giovanni89@gmail.com>
2021-07-21 20:08:26 +0200

    chargement des catégories

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,4 +1,3 @@
-// on ajoute à l'import useEffect de react
 import React, { useState, useEffect } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import axios from 'axios';
@@ -9,15 +8,11 @@ import Footer from 'src/components/Footer';
 import NotFound from 'src/components/NotFound';
 import Spinner from 'src/components/Spinner';
 
-import categoriesData from 'src/data/categories';
+// import categoriesData from 'src/data/categories';
 // import postsData from 'src/data/posts';
 import './styles.scss';
 
 const Blog = () => {
-  //! ATTENTION :
-  // - les hooks ne s'utilisent que dans les composants fonction
-  // - les méthode de lifecycle(cDM, cDU et cWU) ne sont pas disponibles dans un composant fonction
-
   const [zenMode, setZenMode] = useState(false);
 
   const [loading, setLoading] = useState(false);
@@ -25,6 +20,8 @@ const Blog = () => {
   const [hasError, setHasError] = useState(false);
 
   const [posts, setPosts] = useState([]);
+
+  const [categories, setCategories] = useState([]);
 
   const toggleZenMode = () => {
     setZenMode(!zenMode);
@@ -39,15 +36,20 @@ const Blog = () => {
   };
 
   const loadData = async () => {
-    console.log('loadData');
+    // console.log('loadData');
 
     setLoading(true);
 
     setHasError(false);
 
     try {
-      const { data } = await axios.get('https://oclock-open-apis.vercel.app/api/blog/posts');
-      setPosts(data);
+      // on renome data en postsData
+      const { data: postsData } = await axios.get('https://oclock-open-apis.vercel.app/api/blog/posts');
+      setPosts(postsData);
+
+      // on ajoute la requete axios pour categories et on renome data en categoriezdata
+      const { data: categoriesData } = await axios.get('https://oclock-open-apis.vercel.app/api/blog/categories');
+      setCategories(categoriesData);
     }
     catch (error) {
       setHasError(true);
@@ -57,62 +59,25 @@ const Blog = () => {
     }
   };
 
-  // pour exécuter la fonction loadData au 1e rendu du composant, on pouvait le faire
-  // avec la méthode de lifecycle des class componentDidMount
-
-  // ici on donne à useEffect un callback qui sera exécuté
-  // au 1e rendu du composant et à chaque update
-  // => componentDidMount + componentDidUpdate
-  // //? useEffect(() => {
-  //   //? console.log('1e rendu et update du composant');
-  //   //? loadData(); => on part en boucle
-  // //? });
-
-  // callback exécuté uniquement au 1e rendu du composant
-  // il faut mettre un tableau vide en 2e argument de useEffect
-  // => componentDidMount
-  // //? useEffect(() => {
-  //   //? console.log('1e rendu uniquement');
-  //   //? loadData();
-  // //? }, []);
-
-  // callback passé à useEffect sera exécuté au 1e rendu et à l'update du composant
-  // SI la valeur de zenMode change. Pour cela on vient placer la valeur à surveiller
-  // dans le tableau de dépendance en 2e argument
-
-  // => componentDidMount + componentDidUpdate avec condition
-  // //? useEffect(() => {
-  //   //? console.log('1e rendu et quand la valeur de loading change');
-  //   //? loadData(); => quand zenMode change, on relance loadData : pas bon
-  // //? }, [zenMode]);
-
-  // //? useEffect(() => {
-  //   //?   loadData();
-  // //? }, []);
-
-  // ici on a juste l'exécution de la fonction loadData dans le callback
-  // on peut donc passer directement loadData à useEffect
-  // on met un tableau vide en 2e argument pour charger les données uniquement au 1e rendu
   useEffect(loadData, []);
-
-  //! Pour tester dans le navigateur aller dans NetWork et passer "No throttling" à "slow 3G"
-  //! pour pouvoir voir le spinner et les data arriver
 
   return (
     <div className="blog">
       <Header
-        categories={categoriesData}
+        // on retire categoriesData pour méttre categories de la requete
+        categories={categories}
         onClickZenButton={toggleZenMode}
         isZen={zenMode}
       />
-      {/* ici on supprime le bouton qui nous servait de simulateur */}
+
       {loading && <Spinner />}
 
       {hasError && <div>Une erreur s'est produite</div>}
 
       {!loading && !hasError && (
       <Switch>
-        {categoriesData.map(({ route, label }) => (
+        {/* on retire categoriesData pour méttre categories de la requete pour le map */}
+        {categories.map(({ route, label }) => (
           <Route
             key={label}
             exact
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import de DOMPurify de dompurify
 import DOMPurify from 'dompurify';
 
 import './styles.scss';
 
 const Post = ({ category, excerpt, title }) => {
-  // on peut interpréter de l'HTML avec les composants React
-  // il faut utiliser dangerouslySetInnerHTML et lui passer un objet
-  // qui aura la propriété __html
-  // ici on vient créer une fonction qui retourne cette objet
   const createMarkup = (text) => {
-    // on en profite pour assainir l'HTML avec DOMPurify
-    // on évite ainsi les attaques XSS (Cross-Site Scripting)
     const cleanText = DOMPurify.sanitize(text, { ALLOWED_TAGS: ['em', 'strong'] });
 
     return { __html: cleanText };
