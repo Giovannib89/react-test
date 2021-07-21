fd3d32682fdb4eccf59e810ba3150aa38003efc5
Giovannib89 <brugnoni.giovanni89@gmail.com>
2021-07-21 18:17:16 +0200

    utilisation de Redirect

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-// ajout de l'import Switch dans react-router-dom
-import { Route, Switch } from 'react-router-dom';
+// on import redirect dans react router dom
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Header from 'src/components/Header';
 import Posts from 'src/components/Posts';
 import Footer from 'src/components/Footer';
-// importation du composant NotFound
 import NotFound from 'src/components/NotFound';
 
 import categoriesData from 'src/data/categories';
@@ -35,17 +34,7 @@ const Blog = () => {
         onClickZenButton={toggleZenMode}
         isZen={zenMode}
       />
-      {/*
-        Switch permet d'afficher le 1e composant Route
-        dont le path correspondra avec le pathname de l'url
-       */}
       <Switch>
-        {/*
-          ici on génère un composant Route par catégorie
-          pour chacun de ses composants, on va mettre en enfant
-          un composant "Posts" avec en props "posts" une liste
-          filtrée de postsData. Filtrée en fonction de category.label
-        */}
         {categoriesData.map(({ route, label }) => (
           <Route
             key={label}
@@ -55,11 +44,12 @@ const Blog = () => {
             <Posts posts={getPostsByCategory(label, postsData)} isZen={zenMode} />
           </Route>
         ))}
-        {/* ce composant Route ne s'affichera jamais, il y en a un avant lui */}
         <Route path="/angular">Ne s'affiche pas</Route>
 
-        {/* si on veut mettre en place une 404, on ajoute
-        une composant Route sans props "path" à la fin d'un Switch */}
+        {/* Redirect permet de rediriger vers un composant Route
+        qui aura le path préciser dans la prop "to" */}
+        <Redirect from="/jquery" to="/react" />
+
         <Route><NotFound /></Route>
       </Switch>
       <Footer />
